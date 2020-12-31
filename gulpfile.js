b7dbf27e2e374dcda7e14bd4cea9d5928f1c15bc
kais <nagliyavarec@gmail.com>
2020-12-31 15:11:00 +0300

    Update gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 let project_folder = require('path').basename(__dirname);
 let source_folder = '#src';
 let fs = require('fs');
+let style_type = 'sass';
 
-// Пути к файлам
+// Path
 let path = {
 	build: {
 		html: project_folder + '/',
@@ -10,18 +11,24 @@ let path = {
 		js: project_folder + '/js/',
 		img: project_folder + '/img/',
 		fonts: project_folder + '/fonts/',
+		libs: project_folder + '/libs/',
 	},
 	src: {
+		html: source_folder + '/*.html',
 		pug: source_folder + '/pug/*.pug',
-		scss: source_folder + '/scss/style.scss',
-		js: source_folder + '/js/index.js',
+		sass: source_folder + `/sass/*.${style_type}`,
+		css: source_folder + '/css/',
+		js: source_folder + '/js/*.js',
+		jsCopy: source_folder + '/jsCopy/*.js',
 		img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
 		fonts: source_folder + '/fonts/*.ttf',
+		libs: source_folder + '/libs/**/*',
 	},
 	watch: {
+		html: source_folder + '/html/**/*.html',
 		pug: source_folder + '/pug/**/*.pug',
-		scss: source_folder + '/scss/**/*.scss',
-		js: source_folder + '/js/**/*.js',
+		sass: source_folder + `/sass/**/*.${style_type}`,
+		js: source_folder + '/js/*.js',
 		img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
 	},
 	clean: './' + project_folder + '/'
@@ -31,6 +38,8 @@ let path = {
 let {src, dest } = require('gulp'),
 		gulp = require('gulp'),
 		browsersync = require('browser-sync').create(),
+		plumber = require('gulp-plumber'),
+		notify = require('gulp-notify'),
 		pug = require('gulp-pug'),
 		del = require('del'),
 		sass = require('gulp-sass'),
@@ -58,15 +67,25 @@ function browserSync() {
 	})
 }
 
+// HTML
 function html() {
+	return src(path.src.html)
+	.pipe(fileinclude({prefix: '@@',}))
+	.pipe(dest(path.build.html))
+	.pipe(browsersync.stream())
+}
+
+// PUG
+function pugF() {
 	return src(path.src.pug)
 	.pipe(pug({pretty: true}))
 	.pipe(dest(path.build.html))
 	.pipe(browsersync.stream())
 }
 
-function scss() {
-	return src(path.src.scss)
+// SASS
+function sassF() {
+	return src(path.src.sass)
 	.pipe(sourcemaps.init())
 	.pipe(sass({ outputStyle: "expanded" }))
 	.pipe(group_media())
@@ -83,12 +102,22 @@ function scss() {
 	.pipe(browsersync.stream())
 }
 
+// COPY CSS
+function copyCss() {
+	src(path.src.css + 'webfonts/*')
+	.pipe(dest(path.build.css + 'webfonts/'));
+	return src(path.src.css + 'f-a-pro-all.min.css')
+	.pipe(dest(path.build.css + 'font-awesome-css'))
+	.pipe(browsersync.stream())
+}
+
+// JS
 function js() {
 	return src(path.src.js)
 	.pipe(fileinclude())
-	.pipe(babel({
-		presets: ['@babel/env']
-	 }))
+	// .pipe(babel({
+	// 	presets: ['@babel/env']
+	//  }))
 	.pipe(dest(path.build.js))
 	.pipe(uglify())
 	.pipe(rename({ extname: ".min.js" }))
@@ -96,12 +125,28 @@ function js() {
 	.pipe(browsersync.stream())
 }
 
+// COPY JS
+function copyJs() {
+	return src(path.src.jsCopy)
+	.pipe(dest(path.build.js))
+	.pipe(browsersync.stream())
+}
+
+// COPY LIBS
+function libs() {
+	return src(path.src.libs)
+		.pipe(dest(path.build.libs))
+		.pipe(browsersync.stream())
+}
+
+// IMG
 function img() {
 	return src(path.src.img)
 	.pipe(dest(path.build.img))
 	.pipe(browsersync.stream())
 }
 
+// FONTS
 function fonts() {
 	src(path.src.fonts)
 	.pipe(ttf2woff())
@@ -111,6 +156,7 @@ function fonts() {
 	.pipe(dest(path.build.fonts))
 }
 
+// For OTF format
 // Для otf формата
 gulp.task('otf2ttf', function() {
 	return src([source_folder + '/fonts/*.otf'])
@@ -118,11 +164,12 @@ gulp.task('otf2ttf', function() {
 	.pipe(dest(source_folder + '/fonts/'))
 });
 
+// Automatic font connection
 // Автоматическое подключение шрифтов
 function fontStyle() {
-	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+	let file_content = fs.readFileSync(source_folder + '/sass/parts/fonts.sass');
 	if (file_content == '') {
-		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+		fs.writeFile(source_folder + '/sass/parts/fonts.sass', '', cb);
 		return fs.readdir(path.build.fonts, function(err, items) {
 			if (items) {
 				let c_fontname;
@@ -130,7 +177,7 @@ function fontStyle() {
 					let fontname = items[i].split('.');
 					fontname = fontname[0];
 					if (c_fontname != fontname) {
-						fs.appendFile(source_folder + '/scss/fonts.scss', `\n @include font(${fontname}, ${fontname}, 400, normal);`, (err) => {
+						fs.appendFile(source_folder + '/sass/parts/fonts.sass', `\n @include font(${fontname}, ${fontname}, 400, normal);`, (err) => {
 							if (err) throw err;
 							console.log('err')
 						});
@@ -141,36 +188,46 @@ function fontStyle() {
 		})
 	}
 }
+// Needed for the previous function to work correctly
 // Нужно для корректной работы предыдущей функции
 function cb() {}
 
+// CLEAN BUILD DIRECTORY
 function clean() {
 	return del(path.clean);
 }
 
+// WATCH FILES
 function watchFiles() {
-	gulp.watch([path.watch.pug], html);
-	gulp.watch([path.watch.scss], scss);
+	// gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.pug], pugF);
+	gulp.watch([path.watch.sass], sassF);
 	gulp.watch([path.watch.js], js);
+	gulp.watch([path.watch.img], img);
 }
 
 
-let build = gulp.series(clean, gulp.parallel(html, scss, js, fonts), fontStyle);
+
+
+// GULP COMMANDS
+let build = gulp.series(clean, gulp.parallel(pugF, sassF, js, img, fonts, fontStyle));
 let watch = gulp.parallel(browserSync, build, watchFiles);
+
 // EXPPORTS
+exports.libs = libs;
 exports.fontStyle = fontStyle;
+exports.img = img;
 exports.fonts = fonts;
 exports.js = js;
-exports.scss = scss;
+exports.copyJs = copyJs;
+exports.sass = sassF;
+exports.copyCss = copyCss;
+exports.pugF = pugF;
 exports.html = html;
 exports.build = build;
-exports.watch = watch;
 exports.watchFiles = watchFiles;
 exports.default = watch;
-
-
-
-
+exports.clean = clean;
 
 
 
